87e4854b8576465bb41cdb03e4122f5d5a210b79
Revanth Gopi <revanth.1518@gmail.com>
2017-06-10 11:03:11 +0530

    directory changes

--- build.sh
+++ build.sh
@@ -27,11 +27,11 @@ strip_virtualenv () {
     find $VIRTUAL_ENV/lib64/python2.7/site-packages/ -name "*.so" | xargs strip
     echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
 
-    pushd $VIRTUAL_ENV/lib64/python2.7/site-packages/ && zip -r -9 -q ~/outputs/venv.zip * ; popd
-    echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
+    pushd $VIRTUAL_ENV/lib64/python2.7/site-packages/ && zip -r -9 -q ~/sklearn/outputs/venv.zip * ; popd
+    echo "site-packages compressed size $(du -sh ~/sklearn/outputs/venv.zip | cut -f1)"
 
-    pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
-    echo "venv compressed size $(du -sh /outputs/full-venv.zip | cut -f1)"
+    pushd $VIRTUAL_ENV && zip -r -q ~/sklearn/outputs/full-venv.zip * ; popd
+    echo "venv compressed size $(du -sh ~/sklearn/outputs/full-venv.zip | cut -f1)"
 }
 
 shared_libs () {
@@ -44,10 +44,10 @@ shared_libs () {
 
 main () {
     /usr/bin/virtualenv \
-        --python /usr/bin/python ~/sklearn_build \
+        --python /usr/bin/python ~/sklearn/sklearn_build \
         --always-copy \
         --no-site-packages
-    source ~/sklearn_build/bin/activate
+    source ~/sklearn/sklearn_build/bin/activate
 
     do_pip
 
